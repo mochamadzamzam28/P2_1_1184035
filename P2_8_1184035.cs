071f3beef21f4472f8d6295ec42fde04a5c3b70d
mochamadzamzam28 <74168983+mochamadzamzam28@users.noreply.github.com>
2021-11-18 22:24:05 +0700

    Create P2_8_1184035.cs

---
+++ P2_8_1184035.cs
@@ -0,0 +1,29 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace P2_1_1184035
+{
+    public class SHIRT : title;
+    {
+        Protected string 
+        public SHIRT(string type)
+        {
+            this.myTitle = title;
+            this.MyType = "SHIRT"
+        }
+        public string title;
+        {
+            get
+            {
+                retrun type;
+            }
+            Set
+            {
+                PageCount = value;
+            }
+        }
+    }
+}
