e160e1a02ed11c6d958e6432b29ff6a7513a2b21
mochamadzamzam28 <74168983+mochamadzamzam28@users.noreply.github.com>
2021-11-18 22:03:40 +0700

    Create P2_3_1184035.cs

---
+++ P2_3_1184035.cs
@@ -0,0 +1,31 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace P2_1_1184035
+{
+    class Product_1184110
+    {
+        private string myType;
+
+        public Product()
+        {
+
+        }
+
+        public string MyType
+        {
+            get
+            {
+                return myType;
+            }
+
+            set
+            {
+                myType = value;
+            }
+        }
+    }
+}
