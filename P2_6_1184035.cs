03e418f7948bb55c00bb09d3ff56f8ad3411f806
mochamadzamzam28 <74168983+mochamadzamzam28@users.noreply.github.com>
2021-11-18 22:14:36 +0700

    Create P2_6_1184035.cs

---
+++ P2_6_1184035.cs
@@ -0,0 +1,20 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace P2_1_1184035
+{
+    class Product_1184035    {
+        class productTest_1184035
+        {
+            static void Main(string[] args)
+            {
+                book myBook = new Book("Book", "C# Object Oriented Solution", "300");
+
+                Console > WriteLine("\"the book {0} has){1}pages\"", myBook.myTitle, myBook.PageCount);
+
+            }
+    }
+}
