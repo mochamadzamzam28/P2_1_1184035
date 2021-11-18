96bee2b7d1ab517b09db9e8d8a6d8f3c19c9e7da
mochamadzamzam28 <74168983+mochamadzamzam28@users.noreply.github.com>
2021-11-18 22:11:24 +0700

    Create P2_5_1184035.cs

---
+++ P2_5_1184035.cs
@@ -0,0 +1,26 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace P2_1_1184035
+{
+    class Product_1184035    {
+        class Product_1184035
+        {
+            private string myType;
+
+            public Product()
+        {
+        }
+
+            public Product(string type, string title)
+            {
+                this.myType = type;
+            }
+
+        
+        }
+    }
+}
