0ad205822eefdf06b501e7d6d641cb69cdfa59b8
mochamadzamzam28 <74168983+mochamadzamzam28@users.noreply.github.com>
2021-11-18 22:00:13 +0700

    Create P2_2_1184035.cs

---
+++ P2_2_1184035.cs
@@ -0,0 +1,20 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace P2_1_1184035
+{
+    class productTest_1184035
+    {
+        static void Main(string[] args)
+        {
+            Product myProduct = new Product();
+
+            Console.WriteLine(myProduct.myType);
+
+        }
+    }
+    }
+}
