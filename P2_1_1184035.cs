0dbf0923d7dc6ef9a3cb33ce10cdd8e95beae80d
mochamadzamzam28 <74168983+mochamadzamzam28@users.noreply.github.com>
2021-11-18 21:58:42 +0700

    Create P2_1_1184035.cs

---
+++ P2_1_1184035.cs
@@ -0,0 +1,16 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace P2_1_1184035
+{
+    public class Product_1184035
+    {
+        Protected string myType = "book";
+        public Product_1184035()
+        {
+        }
+    }
+}
